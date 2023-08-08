a84bde0a94650adf27a2c8ab674d7764f8308457
Dmitry <alitryel@yandex.ru>
2023-08-09 02:10:32 +0300

    По требованию заказчика, изменена функция, теперь вы должны выбрать папку с текстовыми файлами, а не отдельный файл.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import csv
 import tkinter as tk
 from tkinter import filedialog, messagebox
+import os
 
 def txt_to_csv(input_filename, output_filename):
     with open(input_filename, 'r', encoding='utf-8') as txt_file, open(output_filename, 'w', newline='', encoding='utf-8-sig') as csv_file:
@@ -9,39 +10,40 @@ def txt_to_csv(input_filename, output_filename):
             row = [cell.strip() for cell in line.split('\n')]
             csv_writer.writerow(row)
 
-def convert_and_open():
-    input_file = input_path.get()
-    output_file = "output.csv"
+def convert_and_open(input_folder):
+    output_folder = "Converted CSVs"
+    if not os.path.exists(output_folder):
+        os.mkdir(output_folder)
 
-    txt_to_csv(input_file, output_file)
+    for filename in os.listdir(input_folder):
+        if filename.endswith(".txt"):
+            input_file = os.path.join(input_folder, filename)
+            output_file = os.path.join(output_folder, filename.replace(".txt", ".csv"))
+            txt_to_csv(input_file, output_file)
+
     messagebox.showinfo("Успешно", "Конвертация завершена.")
 
-    open_file = messagebox.askyesno("Открыть файл", "Хотите открыть сконвертированный файл?")
-    if open_file:
-        import os
-        os.system(f"start excel {output_file}")
-
-def browse_file():
-    file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
-    if file_path:
-        input_path.set(file_path)
+def browse_folder():
+    folder_path = filedialog.askdirectory()
+    if folder_path:
+        input_path.set(folder_path)
 
 app = tk.Tk()
 app.title("Конвертер TXT в CSV")
-app.geometry("300x100")
+app.geometry("300x150")  # Увеличенный размер окна
 
 input_path = tk.StringVar()
 
-input_label = tk.Label(app, text="Выберите файл TXT:")
+input_label = tk.Label(app, text="Выберите папку:")
 input_label.pack()
 
 input_entry = tk.Entry(app, textvariable=input_path)
 input_entry.pack()
 
-browse_button = tk.Button(app, text="Обзор...", command=browse_file)
+browse_button = tk.Button(app, text="Обзор...", command=browse_folder)
 browse_button.pack()
 
-convert_button = tk.Button(app, text="Конвертировать", command=convert_and_open)
+convert_button = tk.Button(app, text="Конвертировать", command=lambda: convert_and_open(input_path.get()))
 convert_button.pack()
 
 app.mainloop()
